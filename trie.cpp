f5d516a223996fe28f4e5cde76d23866385ab097
MayEnjoyer <lemurkanikitka@mail.ru>
2025-10-14 13:10:04 +0300

    add find(), finish insert()

--- trie.cpp
+++ trie.cpp
@@ -13,8 +13,8 @@ struct trie_node {
         ch = {};
     }
 
-    trie_node(trie_node& par, char& sym) {
-        prefix = par.prefix + sym;
+    trie_node(trie_node* par, char sym) {
+        prefix = par->prefix + sym;
         ch = {};
     }
 
@@ -26,17 +26,46 @@ struct trie_node {
 
 class trie {
 private:
-    trie_node* root;
+    trie_node* root = nullptr;  
+
+    trie_node* _find(trie_node* root, std::string& stem, size_t i) {
+        if(root == nullptr)
+            return;
+
+        size_t j = 0;
+        while(j < root->ch.size()) {
+            if(root->ch[j]->prefix[i] == stem[i])
+                break;
+        }
+
+        if(j < root->ch.size() && i+1 < stem.size()) 
+            return _find(root->ch[j], stem, i+1);
+        else if(j < root->ch.size()) 
+            return root->ch[j];
+        else 
+            return nullptr;
+    }
 
     void _insert(trie_node* root, std::string& stem, size_t i) {
+        if(i >= stem.size())
+            return;
+
         if(root == nullptr) {
             root = new trie_node(stem);
             return;
         }
 
-        size_t j = -1;
-        for(auto it = root->ch.begin(); it != root->ch.end(); it++) {
-            
+        size_t j = 0;
+        while(j < root->ch.size()) {
+            if(root->ch[j]->prefix[i] == stem[i]);
+                break;
+        }
+
+        if(j < root->ch.size()) 
+            _insert(root->ch[j], stem, i+1);
+        else {
+            trie_node* new_ch = new trie_node(root, stem[i]);
+            root->ch.push_back(new_ch);
         }
     }
 
@@ -46,15 +75,19 @@ private:
         free(root);
     }
 public:
-    trie() {}
+    trie() { root = new trie_node; }
 
     ~trie() { _clear(root); } 
-
+    
+    trie_node* find(std::string& stem) { return _find(root, stem, 0); }
+    
     void insert(std::string& stem) { _insert(root, stem, 0); }
 
     void erase(std::string& stem) {}
 
-    void find(std::string& stem) {}
+    bool empty() { return root == nullptr; }
+
+    bool contains(std::string& stem) { return find(stem) != nullptr; }
 
     void clear() { 
         _clear(root); 
