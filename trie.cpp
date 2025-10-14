047fd681c7c4f384f46eda5cf307bb32cf299beb
MayEnjoyer <lemurkanikitka@mail.ru>
2025-10-14 16:26:23 +0300

    add multiple methods

--- trie.cpp
+++ trie.cpp
@@ -1,18 +1,38 @@
 #include <string>
+#include <vector>
 
 #define TRIE
 #define UNIT_TESTS
 #define CH_SIZE  256
 
+using color = enum { white, black };
+
 struct trie_node {
-    std::string prefix;
+    //std::string prefix;
     trie_node* ch[CH_SIZE] = {0};
+    size_t count = 0;
 
-    trie_node() { prefix = ""; }
+    //trie_node() { prefix = ""; }
 
-    trie_node(trie_node* par, char sym) { prefix = par->prefix + sym; }
+    trie_node() {}
 
-    trie_node(std::string& stem) { prefix = stem; }
+    //trie_node(trie_node* par, char sym) { prefix = par->prefix + sym; }
+
+    //trie_node(std::string& stem) { prefix = stem; }
+
+    bool is_leaf() {
+        if(this == nullptr) {
+            return true;
+        }
+
+        for(size_t i = 0; i < CH_SIZE; i++) {
+            if(this->ch[i] != nullptr) {
+                return false;
+            }
+        }
+
+        return true;
+    }
 };
 
 class trie {
@@ -51,12 +71,35 @@ private:
         }
     }
 
-    void _push_prefix(trie_node* st_node, std::string& stem, size_t st) {
-        if(st == stem.size())
+    void _erase(trie_node* root, std::string& stem, size_t i) {
+        if(root == nullptr) {
             return;
+        }
+        
+        if(i == stem.size()) {
+            root->count -= 1;
+            return;
+        }
+
+        if(root->ch[stem[i]] != nullptr) {
+            _erase(root->ch[stem[i]], stem, i+1);
+            if(root->ch[stem[i]]->is_leaf() == true) {
+                delete root->ch[i];
+                root->ch[stem[i]] = nullptr;
+            }
+        } 
+        
+    }
+
+    void _push_prefix(trie_node* st_node, std::string& stem, size_t st) {
+        if(st == stem.size()) {
+            st_node->count += 1;
+            return;
+        }
+
         st_node->ch[stem[st]] = new trie_node;
         _push_prefix(st_node->ch[stem[st]], stem, st+1);
-    }
+    } 
 
     void _clear(trie_node* root) {
         if(root == nullptr) {
@@ -80,11 +123,43 @@ public:
     
     void insert(std::string& stem) { _insert(root, stem, 0); }
 
-    void erase(std::string& stem) {}
+    void erase(std::string& stem) { _erase(root, stem, 0); }
 
     bool empty() { return ch_count == 0; }
 
-    bool contains(std::string& stem) { return find(stem) != nullptr; }
+    bool contains(std::string& stem) { 
+        trie_node* node = find(stem);
+
+        if(node == nullptr) {
+            return false;
+        } else {
+            return node->count != 0;
+        }
+    }
+
+    size_t count(std::string& stem) { 
+        trie_node* node = find(stem);
+
+        if(node == nullptr) {
+            return 0;
+        } else {
+            return node->count;
+        }
+    }
+
+    size_t count(std::vector<std::string>& stems) {
+        size_t ans = 0;
+        trie_node* temp = nullptr;
+
+        for(size_t i = 0; i < stems.size(); i++) {
+            temp = this->find(stems[i]);
+            if(temp != nullptr) {
+                ans += temp->count;
+            }
+        }
+
+        return ans;
+    }
 
     void clear() { 
         _clear(root); 
@@ -117,6 +192,19 @@ int main(void)
     assert(T.contains(_b) == true);
     assert(T.find(_c) != nullptr);
     assert(T.find(_d) == nullptr);
+
+    assert(T.count(_b) == 1);
+
+    T.erase(_b);
+    T.erase(_d);
+    
+    assert(T.count(_b) == 0);
+    assert(T.count(_c) == 1);
+    assert(T.count(_a) == 1);
+    assert(T.count(_d) == 0);
+
+    std::vector<std::string> _abcd = {_a, _b, _c, _d};
+    assert(T.count(_abcd) == 2);
 
     T.clear();
 
